028c329ac427d0f4cecd49e9844245c77555115e
Andriy Fuji Pendloski <afp@edu.univali.br>
2020-06-25 20:24:55 -0300

    Data input and setting the sucessors and predecessors

--- main.cpp
+++ main.cpp
@@ -15,6 +15,64 @@ struct activity
     vector<string> predecessors;
 };
 
+//Checks if the given ID exists or not, and if it's not the same as the one being changed
+bool checkActivityId(vector<activity> activities, string id, int currentActivity)
+{
+    for (int i = 0; i < activities.size(); i++)
+        if(activities[i].id == id && activities[currentActivity].id != id)
+            return true;
+    
+    return false;
+}
+
+void setSucessor(vector<activity> &activities, string id, string currentActivityId)
+{
+    for (int i = 0; i < activities.size(); i++)
+    {
+        if(activities[i].id == id)
+            activities[i].successors.push_back(currentActivityId);
+    }
+    
+}
+
+//In
+void activitiesInput(vector<activity> &activities)
+{
+    for (int i = 0; i < activities.size(); i++)
+    {
+        cout << "\nActivity " << i+1 << "\n\n";
+        
+        cout << "ID: "; 
+        cin >> activities[i].id;
+
+        cout << "Duration: ";
+        cin >> activities[i].duration;
+
+        int numberPredecessors;
+        cout << "Number of predecessors: ";
+        cin >> numberPredecessors;
+        for (int j = 0; j < numberPredecessors; j++)
+        {
+            string activityId;
+            cout << "Predecessor ID #" << j+1 << ": ";
+            cin >> activityId;
+            if(checkActivityId(activities, activityId, i))
+            {
+                //Sets the predecessor
+                activities[i].predecessors.push_back(activityId);
+                //Sets the sucessor
+                setSucessor(activities, activityId, activities[i].id);
+
+            }
+            else
+            {
+                cout << "\n ID not found! Try again.\n\n";
+                j--;
+            }
+        }
+    }
+}
+
 int main()
 {
     int numberActivities;
@@ -23,4 +81,17 @@ int main()
 
     vector<activity> activities(numberActivities);
 
+    activitiesInput(activities);
+
+    //checking if predecessors and sucessors are working
+    /*for (int i = 0; i < numberActivities; i++)
+    {
+        cout << '\n';
+        for (int j = 0; j < activities[i].predecessors.size(); j++)
+            cout << "predecessors: " << activities[i].predecessors[j] << ' ';
+        cout << '\n';
+        for (int j = 0; j < activities[i].successors.size(); j++)
+            cout << "sucessors: " << activities[i].successors[j] << ' ';
+    }*/
+    system("pause");
 }
